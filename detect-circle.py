46b497e9c32bb20b4739003688452acb0b9edf31
neokkk <seven3126@gmail.com>
2023-04-25 00:00:20 +0900

    feat: 원을 감지해 LED 제어

---
+++ detect-circle.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+import sys
+import RPi.GPIO as gpio
+
+pinNum = int(sys.argv[1])
+
+if not pinNum:
+  exit(-1)
+
+gpio.setmode(gpio.BOARD)
+gpio.setup(pinNum, gpio.OUT)
+videoCapture = cv.VideoCapture(0, cv.CAP_V4L)
+
+def clean():
+  videoCapture.release()
+  gpio.cleanup()
+
+try:
+  while True:
+    ret, frame = videoCapture.read()
+    if not ret: break
+
+    cv.imshow('frame', frame)
+    key = cv.waitKey(10)
+
+    if key == 27:
+      break
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # grayscale
+    blur = cv.GaussianBlur(gray, (15, 15), 0)
+
+    circles = cv.HoughCircles(blur, cv.HOUGH_GRADIENT, 2, 100, param1=100, param2=100, minRadius=35, maxRadius=500) # Hough Transform
+
+    if circles is not None:
+      gpio.output(pinNum, gpio.HIGH)
+    else:
+      gpio.output(pinNum, gpio.LOW)
+except KeyboardInterrupt:
+  clean
+
+clean
